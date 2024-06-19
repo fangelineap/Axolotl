e26511bc67ba518682fa29d74833d85e66d40788
Fangeline <fangelineap@gmail.com>
2024-06-19 17:08:13 +0700

    Fix: navbar & sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -66,7 +66,7 @@ const Header = (props: {
           </Link>
         </div>
 
-        <div className="hidden xl:block">
+        <div className="hidden lg:block">
           <div>
             <h1 className="mb-0.5 text-heading-5 font-bold text-dark dark:text-white">
               Dashboard
@@ -75,7 +75,7 @@ const Header = (props: {
           </div>
         </div>
 
-        <div className="flex items-center justify-normal gap-2 2xsm:gap-4 lg:w-full lg:justify-between xl:w-auto xl:justify-normal">
+        <div className="flex items-center justify-normal gap-2 2xsm:gap-4 lg:w-full lg:justify-end xl:w-auto xl:justify-normal">
           <ul className="flex items-center gap-2 2xsm:gap-4">
             {/* <!-- Search Form --> */}
             <SearchForm />
