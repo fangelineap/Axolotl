6fab247c60b62acc47e268b5653cb21d296003ae
Arvel Kartawijaya <arvel.kartawijaya@binus.ac.id>
2024-06-21 17:58:06 +0700

    Fix footer position

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,48 +4,76 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full border-t border-stroke bg-white dark:border-stroke-dark dark:bg-gray-dark">
-      <div className="flex flex-col items-center justify-center px-5 py-7 md:flex-row md:px-2 2xl:px-10">
-        <div className="flex flex-col items-center py-7 md:items-start">
-          <Link href="/">
-            <div className="cursor-pointer">
+    <footer className="relative w-full border-t border-stroke bg-white dark:border-stroke-dark dark:bg-gray-dark">
+      <div className="flex flex-col justify-between px-5 pt-10 md:flex-row md:px-2 2xl:px-10">
+        <div className="flex flex-col px-25 md:flex-row md:items-start">
+          <div className="flex flex-col items-start">
+            <Link href="/">
+              <div className="cursor-pointer">
+                <Image
+                  width={250}
+                  height={250}
+                  src="/images/logo/axolotl-kalbe.svg"
+                  alt="Logo"
+                />
+              </div>
+            </Link>
+            <span className="font-extrabold">
+              To improve Health for a Better Live, by
+            </span>
+            <span>Prioritizing your health at your house.</span>
+            <div className="py-6">
               <Image
-                width={250}
-                height={250}
-                src="/images/logo/axolotl-kalbe.svg"
-                alt="Logo"
+                width={200}
+                height={200}
+                src="/images/logo/copyright.svg"
+                alt="Copyright"
               />
             </div>
-          </Link>
-          <div className="py-3">
-            <Image
-              width={90}
-              height={90}
-              src="/images/logo/copyright.svg"
-              alt="Copyright"
-            />
+          </div>
+          <div className="mt-7 flex flex-col items-start md:ml-20 md:mt-0">
+            <span className="text-lg text-gray-800 dark:text-white">
+              Company
+            </span>
+            <span className="mt-2 text-gray-500 dark:text-gray-400">
+              About Us
+            </span>
+            <span className="mt-2 text-gray-500 dark:text-gray-400">
+              Careers
+            </span>
+            <span className="mt-2 text-gray-500 dark:text-gray-400">
+              Security
+            </span>
+            <span className="mt-2 text-gray-500 dark:text-gray-400">
+              Term & Privacy
+            </span>
           </div>
         </div>
-        <div className="mt-7 flex flex-col items-center md:ml-20 md:mt-0 md:items-start">
-          <span className="text-lg text-gray-800 dark:text-white">Company</span>
-          <span className="mt-2 text-gray-500 dark:text-gray-400">
-            About Us
-          </span>
-          <span className="mt-2 text-gray-500 dark:text-gray-400">Careers</span>
-          <span className="mt-2 text-gray-500 dark:text-gray-400">
-            Security
-          </span>
-          <span className="mt-2 text-gray-500 dark:text-gray-400">
-            Term & Privacy
-          </span>
-        </div>
-        <div className="mt-7 flex flex-col items-center md:ml-20  md:mt-0 md:items-start">
-          <span className="text-lg text-gray-800 dark:text-white">
-            Resources
-          </span>
-          <span className="mt-2 text-gray-500 dark:text-gray-400">
-            Help Center
-          </span>
+        <div className="absolute right-5  flex space-x-3">
+          <Image
+            width={20}
+            height={20}
+            src="/images/icon/instagram.svg"
+            alt="Instagram"
+          />
+          <Image
+            width={20}
+            height={20}
+            src="/images/icon/twitter.svg"
+            alt="Twitter"
+          />
+          <Image
+            width={20}
+            height={20}
+            src="/images/icon/linkedin.svg"
+            alt="Linkedin"
+          />
+          <Image
+            width={20}
+            height={20}
+            src="/images/icon/youtube.svg"
+            alt="Youtube"
+          />
         </div>
       </div>
     </footer>
