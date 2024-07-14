5330b3bf21302570b703da96a261562af7b08dc5
Arvel Kartawijaya <arvel.kartawijaya@binus.ac.id>
2024-07-14 10:56:53 +0700

    merge

--- src/components/Layouts/DefaultLaout.tsx
+++ src/components/Layouts/DefaultLaout.tsx
@@ -12,7 +12,6 @@ export default function DefaultLayout({
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-<<<<<<< HEAD
     <div className="flex h-screen overflow-hidden">
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
       <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
@@ -26,34 +25,6 @@ export default function DefaultLayout({
           </div>
         </main>
         <Footer />
-=======
-    <>
-      {/* <!-- ===== Page Wrapper Star ===== --> */}
-      <div className="flex h-screen overflow-hidden">
-        {/* <!-- ===== Sidebar Star ===== --> */}
-        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-        {/* <!-- ===== Sidebar End ===== --> */}
-
-        {/* <!-- ===== Content Area Star ===== --> */}
-        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-          {/* <!-- ===== Header Star ===== --> */}
-          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-          {/* <!-- ===== Header End ===== --> */}
-
-          {/* <!-- ===== Main Content Star ===== --> */}
-          <main className="flex-grow">
-            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-              {children}
-            </div>
-          </main>
-          {/* <!-- ===== Main Content End ===== --> */}
-
-          {/* <!-- ===== Footer Start ===== --> */}
-          <Footer />
-          {/* <!-- ===== Footer End ===== --> */}
-        </div>
-        {/* <!-- ===== Content Area End ===== --> */}
->>>>>>> 7b761bd6441355c536ab2f4e301495cf112185d2
       </div>
     </div>
   );
