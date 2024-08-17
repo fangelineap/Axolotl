8ed9723a94247b6919fee9aa325ce131357f06b8
Iannn <pristianb007@gmail.com>
2024-08-17 22:56:10 +0700

    Added CG Pages

--- src/app/(pages)/caregiver/page.tsx
+++ src/app/(pages)/caregiver/page.tsx
@@ -1,10 +1,272 @@
+"use client";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
-import React from "react";
+import React, { useState } from "react";
+import Image from "next/image";
+import ReactModal from "react-modal";
+
+// Helper function to format the date as 'Saturday, July 20'
+const formatDate = (date: Date) => {
+  const options: Intl.DateTimeFormatOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return date.toLocaleDateString("en-US", options);
+};
 
 const Dashboard = () => {
+  const today = new Date();
+  const formattedToday = formatDate(today);
+
+  // Example of adding a future date for demonstration purposes
+  const futureDate = new Date(today);
+  futureDate.setDate(today.getDate() + 12); // Add 12 days to the current date
+  const formattedFutureDate = formatDate(futureDate);
+
+  const appointments = [
+    {
+      date: formattedToday,
+      appointments: [],
+    },
+    {
+      date: formattedFutureDate,
+      appointments: [
+        {
+          title: "After Care",
+          description: "Wound Treatment",
+          provider: "Axolotl",
+          startTime: `${futureDate.toLocaleDateString()} 23:59`,
+          endTime: `${new Date(futureDate.getTime() + 86400000).toLocaleDateString()} 23:59`,
+          location: "Jl. Lorem Ipsum, Malang City, East Java, Indonesia",
+        },
+        {
+          title: "Booster",
+          description: "Vitamin Booster",
+          provider: "Axolotl",
+          startTime: `${futureDate.toLocaleDateString()} 23:59`,
+          endTime: `${new Date(futureDate.getTime() + 86400000).toLocaleDateString()} 23:59`,
+          location: "Jl. Lorem Ipsum, Malang City, East Java, Indonesia",
+        },
+        {
+          title: "Elder",
+          description: "Basic threatment",
+          provider: "Arvel",
+          startTime: `${futureDate.toLocaleDateString()} 23:59`,
+          endTime: `${new Date(futureDate.getTime() + 86400000).toLocaleDateString()} 23:59`,
+          location: "Jl. Gajah Mada, Batu City, East Java, Indonesia",
+        },
+      ],
+    },
+  ];
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedAppointment, setSelectedAppointment] = useState(null);
+  const [reasonModalIsOpen, setReasonModalIsOpen] = useState(false);
+  const [cancellationReason, setCancellationReason] = useState("");
+
+  const openModal = (appointment) => {
+    setSelectedAppointment(appointment);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+    setSelectedAppointment(null);
+  };
+
+  const openReasonModal = () => {
+    setModalIsOpen(false); // Close the first modal
+    setReasonModalIsOpen(true); // Open the second modal
+  };
+
+  const closeReasonModal = () => {
+    setReasonModalIsOpen(false);
+  };
+
+  const handleCancel = () => {
+    console.log("Cancel appointment", selectedAppointment);
+    openReasonModal();
+  };
+
+  const handleFinalCancel = () => {
+    console.log("Reason for cancellation:", cancellationReason);
+    closeReasonModal();
+    // Further logic to handle the cancellation can be added here
+  };
+
   return (
     <DefaultLayout>
-      <div>Dashboard</div>
+      <nav className="mb-2 text-sm text-gray-600">Dashboard / Schedule</nav>
+      <h1 className="text-5xl font-bold">Schedule</h1>
+      {appointments.map((day) => (
+        <div key={day.date} className="mt-8">
+          <h2
+            className={`text-3xl font-medium sm:text-2xl ${day.appointments.length === 0 ? "rounded-lg border-0 bg-kalbe-ultraLight pb-2 pl-5 pt-2 text-kalbe-light" : "rounded-lg border-0 bg-gray pb-2 pl-5 pt-2 text-gray-800"}`}
+          >
+            {day.date}{" "}
+            {day.date === formattedToday && (
+              <span className="text-kalbe-light">| Today</span>
+            )}
+          </h2>
+
+          {day.appointments.length === 0 ? (
+            <div className="ml-3 mr-3 mt-4 rounded-lg border border-gray-300 p-4 text-center">
+              You don&apos;t have any appointment today
+            </div>
+          ) : (
+            day.appointments.map((appointment, index) => (
+              <div
+                key={index}
+                className="ml-3 mr-3 mt-4 rounded-lg border border-gray-300 p-4"
+              >
+                <div className="flex flex-col lg:flex-row lg:justify-between">
+                  <div className=" flex-1 lg:mb-0">
+                    <h3 className="text-lg font-bold">{appointment.title}</h3>
+                    <p className="text-sm text-gray-500">
+                      {appointment.description}
+                    </p>
+                    <div className="mb-2 mt-1 text-sm text-gray-600">
+                      <div className="flex items-center">
+                        <Image
+                          width={16}
+                          height={16}
+                          src="/images/icon/profile-icon.svg"
+                          alt="Profile"
+                        />
+                        <span className="ml-2">{appointment.provider}</span>
+                      </div>
+                      <div className="flex items-center">
+                        <Image
+                          width={16}
+                          height={16}
+                          src="/images/icon/clock-icon.svg"
+                          alt="Calendar"
+                        />
+                        <span className="ml-2">
+                          {appointment.startTime} - {appointment.endTime}
+                        </span>
+                      </div>
+                      <div className=" flex items-center">
+                        <Image
+                          width={16}
+                          height={16}
+                          src="/images/icon/location-icon.svg"
+                          alt="Location"
+                        />
+                        <span className="ml-2">{appointment.location}</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex flex-col sm:p-3 lg:mb-0 lg:flex-row lg:items-start lg:space-x-4">
+                    <button className="mb-2 mt-0 rounded-lg border border-green px-4 py-2 text-sm font-bold text-kalbe-light hover:border-none hover:bg-green-success-hover">
+                      See more
+                    </button>
+                    <div className="hidden lg:block lg:h-20 lg:border-l lg:border-gray-400"></div>
+
+                    <div className="flex flex-col">
+                      <button
+                        className="b mb-2 rounded-lg border bg-gray-cancel px-4 py-2 text-sm font-bold text-white hover:bg-gray-cancel-hover hover:text-black"
+                        onClick={() => openModal(appointment)}
+                      >
+                        Cancel Appointment
+                      </button>
+
+                      <button className="mb-2 rounded-lg bg-green-success px-4 py-2 text-sm font-bold text-white hover:bg-green-success-hover hover:text-green">
+                        Medicine Preparation
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))
+          )}
+        </div>
+      ))}
+
+      {/* First Modal */}
+      <ReactModal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Cancel Appointment"
+        className="fixed inset-0 z-10 flex items-center justify-center bg-gray-800 bg-opacity-75 p-4"
+      >
+        <div className="w-full max-w-md rounded-lg bg-white p-6 shadow-lg">
+          <h2 className="mb-4 text-xl font-bold">Confirmation</h2>
+          {selectedAppointment && (
+            <div>
+              <p className="mb-4">
+                Are you sure you want to cancel this appointment?
+              </p>
+              <p className="mb-2">
+                <strong>{selectedAppointment.title}</strong>
+              </p>
+              <p className="text-sm text-gray-500">
+                {selectedAppointment.description}
+              </p>
+              <p className="text-sm text-gray-500">
+                {selectedAppointment.provider}
+              </p>
+              <p className="text-sm text-gray-500">
+                {selectedAppointment.startTime} - {selectedAppointment.endTime}
+              </p>
+              <p className="text-sm text-gray-500">
+                {selectedAppointment.location}
+              </p>
+            </div>
+          )}
+          <div className="mt-6 flex justify-end">
+            <button
+              className="mr-4 rounded-lg bg-gray-cancel px-4 py-2 text-sm font-bold text-white hover:bg-gray-cancel-hover hover:text-black"
+              onClick={closeModal}
+            >
+              No, cancel
+            </button>
+            <button
+              className="rounded-lg bg-red px-4 py-2 text-sm font-bold text-white hover:bg-red-hover hover:text-red"
+              onClick={handleCancel}
+            >
+              Yes, I&apos;m sure
+            </button>
+          </div>
+        </div>
+      </ReactModal>
+
+      {/* Second Modal */}
+      <ReactModal
+        isOpen={reasonModalIsOpen}
+        onRequestClose={closeReasonModal}
+        contentLabel="Cancellation Reason"
+        className="fixed inset-0 z-10 flex items-center justify-center bg-gray-800 bg-opacity-75 p-4"
+      >
+        <div className="w-full max-w-md rounded-lg bg-white p-6 shadow-lg">
+          <h2 className="mb-4 text-xl font-bold">Confirmation</h2>
+          <p className="mb-4">
+            To confirm, type your reason why you cancel this appointment
+          </p>
+          <textarea
+            className="w-full rounded-lg border border-gray-300 p-2"
+            rows={4}
+            placeholder="Type your reason here..."
+            value={cancellationReason}
+            onChange={(e) => setCancellationReason(e.target.value)}
+          />
+          <div className="mt-6 flex justify-end">
+            <button
+              className="mr-4 rounded-lg bg-gray-cancel px-4 py-2 text-sm font-bold text-white hover:bg-gray-cancel-hover hover:text-black"
+              onClick={closeReasonModal}
+            >
+              Cancel
+            </button>
+            <button
+              className="rounded-lg bg-red px-4 py-2 text-sm font-bold text-white hover:bg-red-hover hover:text-red"
+              onClick={handleFinalCancel}
+            >
+              Cancel this appointment
+            </button>
+          </div>
+        </div>
+      </ReactModal>
     </DefaultLayout>
   );
 };
---
+++ src/app/(pages)/caregiver/profile/page.tsx
@@ -0,0 +1,199 @@
+"use client";
+
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import React, { useState } from "react";
+import InputGroup from "@/components/FormElements/InputGroup";
+import SelectGroupTwo from "@/components/FormElements/SelectGroup/SelectGroupTwo";
+import DatePickerOne from "@/components/FormElements/DatePicker/DatePickerOne";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+
+const CaregiverProfile = () => {
+  const router = useRouter();
+  const [profileData, setProfileData] = useState({
+    phoneNumber: "",
+    birthdate: "",
+    address: "",
+    startDay: "Monday",
+    endDay: "Friday",
+    startTime: "18:30",
+    endTime: "21:00",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setProfileData({
+      ...profileData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Profile data submitted:", profileData);
+    // Further logic to handle the form submission can be added here
+  };
+
+  return (
+    <DefaultLayout>
+      <div className="sm:p-6 md:p-2">
+        <nav className="mb-2 text-sm text-gray-600">Dashboard / Profile</nav>
+        <h1 className="text-3xl font-bold md:text-5xl">Your Profile</h1>
+
+        {/* Profile Header Section */}
+        <div className="mt-6 flex items-center gap-4">
+          <Image
+            src="/images/logo/axolotlonly.svg" // Replace with the actual path to the avatar image
+            alt="Avatar"
+            className="rounded-full"
+            width="150"
+            height="150"
+          />
+          <div>
+            <h2 className="text-2xl font-bold">Barry Allen</h2>
+            <p className="text-gray-500">barry.allen@axolotl.com</p>
+            <button className="mt-2 rounded-lg bg-kalbe-light px-4 py-2 font-bold text-white">
+              Edit Profile
+            </button>
+          </div>
+        </div>
+
+        <form
+          onSubmit={handleSubmit}
+          className="mt-8 flex flex-col gap-8 md:flex-row"
+        >
+          {/* About You Section */}
+          <div className="flex-1">
+            <h2 className="mb-4 text-xl font-bold text-kalbe-light">
+              About You
+            </h2>
+            <div className="pb-3">
+              <InputGroup
+                name="phoneNumber"
+                label="Phone Number"
+                type="text"
+                value={profileData.phoneNumber}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+
+            <div className="pb-3">
+              <DatePickerOne
+                name="birthdate"
+                label="Birthdate"
+                value={profileData.birthdate}
+                onChange={(date) =>
+                  setProfileData({ ...profileData, birthdate: date })
+                }
+                required
+              />
+            </div>
+
+            <div className="pb-3">
+              <InputGroup
+                name="address"
+                label="Address"
+                type="text"
+                placeholder="Jl. Lorem Ipsum, Malang City, East Java, Indonesia"
+                value={profileData.address}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+          </div>
+
+          {/* Vertical Separator Line */}
+          <div className="hidden w-[1px] bg-primary md:block"></div>
+
+          {/* Working Preferences Section */}
+          <div className="flex-1">
+            <h2 className="mb-4 text-xl font-bold text-kalbe-light">
+              Working Preferences
+            </h2>
+            <div className="mb-3 flex flex-row gap-5">
+              <div className="flex-1">
+                <SelectGroupTwo
+                  name="startDay"
+                  label="Start Day"
+                  content={[
+                    "Monday",
+                    "Tuesday",
+                    "Wednesday",
+                    "Thursday",
+                    "Friday",
+                    "Saturday",
+                    "Sunday",
+                  ]}
+                  value={profileData.startDay}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+              <div className="flex-1">
+                <SelectGroupTwo
+                  name="endDay"
+                  label="End Day"
+                  content={[
+                    "Monday",
+                    "Tuesday",
+                    "Wednesday",
+                    "Thursday",
+                    "Friday",
+                    "Saturday",
+                    "Sunday",
+                  ]}
+                  value={profileData.endDay}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+            </div>
+
+            <div className="mb-3 flex flex-row gap-5">
+              <div className="flex-1">
+                <InputGroup
+                  name="startTime"
+                  label="Start Time"
+                  type="time"
+                  value={profileData.startTime}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+              <div className="flex-1">
+                <InputGroup
+                  name="endTime"
+                  label="End Time"
+                  type="time"
+                  value={profileData.endTime}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+            </div>
+          </div>
+        </form>
+
+        {/* Submit Buttons */}
+        <div className="mt-8 flex justify-start gap-4 sm:flex-row">
+          <button
+            type="button"
+            onClick={() => router.back()}
+            className="rounded-lg bg-gray-200 px-4 py-2 font-bold text-gray-600"
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="rounded-lg bg-kalbe-light px-4 py-2 font-bold text-white"
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    </DefaultLayout>
+  );
+};
+
+export default CaregiverProfile;
