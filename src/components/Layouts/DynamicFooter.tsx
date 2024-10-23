0d13e971f4b812fbd61e56a56d9fcdb033815572
Iannn <pristianb007@gmail.com>
2024-10-23 20:30:39 +0700

    Order Patient Footer Condition Update

--- src/components/Layouts/DynamicFooter.tsx
+++ src/components/Layouts/DynamicFooter.tsx
@@ -4,29 +4,25 @@ import React from "react";
 import { usePathname } from "next/navigation";
 import CGAdminFooter from "../Footer/CGAdmin";
 import GuestPatientFooter from "../Footer/GuestPatient";
+import OrderPatientFooter from "../Footer/OrderPatient";
 
 const DynamicFooter = () => {
   const pathname = usePathname();
 
-  if (pathname.startsWith("/admin") || pathname.startsWith("/caregiver")) {
-    return (
-      <>
-        <CGAdminFooter />
-      </>
-    );
-  }
+  if (pathname.startsWith("/admin") || pathname.startsWith("/caregiver"))
+    return <CGAdminFooter />;
 
-  if (pathname.startsWith("/auth")) {
-    return null;
-  }
+  if (pathname.startsWith("/auth")) return null;
 
-  if (pathname.startsWith("/patient") || pathname.startsWith("/")) {
-    return (
-      <>
-        <GuestPatientFooter />
-      </>
-    );
-  }
+  if (
+    (pathname.startsWith("/patient") &&
+      !pathname.includes("health-services")) ||
+    pathname.startsWith("/")
+  )
+    return <GuestPatientFooter />;
+
+  if (pathname.startsWith("/patient") && !pathname.includes("health-services"))
+    return <OrderPatientFooter />;
 };
 
 export default DynamicFooter;
