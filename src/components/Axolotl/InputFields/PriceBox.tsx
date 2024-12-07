c81d56f8a2d591e7f35c94523c32fabff4b93314
Arvel Kartawijaya <arvel.kartawijaya@binus.ac.id>
2024-12-07 17:13:41 +0700

    fix conflict

--- src/components/Axolotl/InputFields/PriceBox.tsx
+++ src/components/Axolotl/InputFields/PriceBox.tsx
@@ -39,49 +39,28 @@ const PriceBox = ({
         {required && <span className="ml-1 text-red">*</span>}
       </label>
       <div className="flex w-full items-center">
-        <div className="flex w-full items-center">
-          {!isViewOnly && (
-            <span className="flex-none rounded-l-md border border-r-0 border-gray-1 bg-gray px-2 py-2 font-normal text-dark-secondary dark:text-white">
-              Rp.
-            </span>
-          )}
-          <input
-            name={name}
-            aria-label="Price"
-            type="text"
-            placeholder={placeholder}
-            value={value === 0 ? "" : value?.toString()}
-            onChange={handleInputChange}
-            disabled={disabled}
-            maxLength={8}
-            className={`w-full rounded-md border border-gray-1 bg-white px-2 py-2 font-normal text-dark outline-none transition ${
-              disabled
-                ? "disabled:cursor-default disabled:bg-gray disabled:text-dark-secondary"
-                : "bg-white"
-            }`}
-          />
-        ) : (
-          <div className="flex w-full items-center">
-            <span className="rounded-l-md border border-r-0 border-gray-1 bg-gray px-1 py-2 font-normal text-dark-secondary dark:text-white">
-              Rp.
-            </span>
-            <input
-              name={name}
-              aria-label="Price"
-              type="text"
-              placeholder={placeholder}
-              value={value === 0 ? "" : value?.toString()}
-              onChange={handleInputChange}
-              disabled={disabled}
-              maxLength={12} // Disesuaikan sesuai kebutuhan
-              className={`flex-1 rounded-r-md border border-gray-1 bg-white px-2 py-2 font-normal text-dark outline-none transition ${
-                disabled
-                  ? "disabled:cursor-default disabled:bg-gray disabled:text-dark-secondary"
-                  : "bg-white"
-              }`}
-            />
-          </div>
+        {!isViewOnly && (
+          <span className="flex-none rounded-l-md border border-r-0 border-gray-1 bg-gray px-2 py-2 font-normal text-dark-secondary dark:text-white">
+            Rp.
+          </span>
         )}
+        <input
+          name={name}
+          aria-label="Price"
+          type="text"
+          placeholder={placeholder}
+          value={value === 0 ? "" : value?.toString()}
+          onChange={handleInputChange}
+          disabled={disabled || isViewOnly}
+          maxLength={12}
+          className={`${
+            isViewOnly ? "cursor-not-allowed bg-gray-200" : "bg-white"
+          } flex-1 rounded-r-md border border-gray-1 px-2 py-2 font-normal text-dark outline-none transition ${
+            disabled
+              ? "disabled:cursor-default disabled:bg-gray disabled:text-dark-secondary"
+              : ""
+          }`}
+        />
       </div>
     </div>
   );
